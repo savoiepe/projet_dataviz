db3d1eabc6edad14f9b75018f7ec2d3d4f9d34a5
joel2lin <129231242+joel2lin@users.noreply.github.com>
2023-04-05 00:33:06 +0200

    Update second_graph.py
    ajout des titres selon le bouton cliqué

--- src/second_graph.py
+++ src/second_graph.py
@@ -7,7 +7,6 @@ import preprocess
 def get_figure(data, selected, year):
     '''
         Function to get the figure with given metrics
-
         data is the complete data
         metric is the metric of magitude to use in the graph, it can be either 'nbVues', 'nbCommentaires', 'nbLikes' or 'nbPartages'
         group_by_column is the column to consider, it can be either "compte", "durée" ou "tags"
@@ -16,4 +15,14 @@ def get_figure(data, selected, year):
 
     data_to_display = preprocess.line_graph(data, selected, year)
     fig = px.line(data_to_display, x="date_str", y="n_post")
+    if selected is None:
+        fig.update_layout(title='Nombre de tiktok posté entre 2019 et 2022',title_x=0.5)
+        return fig
+    if selected[0]=='durée':
+        fig.update_layout(title='Nombre de tiktoks posté de durée '+ selected[1] + ' secondes')
+    elif selected[0]=='tags':
+        fig.update_layout(title='Nombre de tiktoks posté ayant pour sujet '+ "'" + selected[1] + "'")
+    elif selected[0]=='compte':
+        fig.update_layout(title='Nombre de tiktoks posté par le média '+ selected[1])
+    fig.update_layout(title_x=0.5)
     return fig 
