5760278d2421349acb4d056181593eea23e9fbc4
GuillBla <guillaume.blanche@etu.emse.fr>
2023-04-05 01:09:16 -0400

    Update second_graph.py

--- src/second_graph.py
+++ src/second_graph.py
@@ -15,52 +15,26 @@ def get_figure(data, selected, year):
     
     data_to_display = preprocess.line_graph(data, selected, year)
     fig = px.line(data_to_display, x="date_str", y="n_post")
+    fig.update_xaxes(
+    showticklabels=True,
+    tickformat='%Y-%m',
+    ticklen=10,
+    tickangle=0,
+    showgrid=False,
+    showline=True,
+    linewidth=1,
+    linecolor='black',
+    mirror=True
+)   
     
-    fig.add_shape(
-        type='line',
-        x0='2020-03-01', x1='2020-03-01', xref='x', y0=0, y1=1, yref='paper',
-        line=dict(color='red', width=2, dash='dash')
-    )
-    
-    fig.add_shape(
-        type='line',
-        x0='2022-03-01', x1='2022-03-01', xref='x', y0=0, y1=1, yref='paper',
-        line=dict(color='green', width=2, dash='dash')
-    )
-    
-    
-    
-    # Ajouter une légende avec le style de ligne
-    fig.add_shape(
-        type='rect',
-        x0=0.05, x1=0.28, xref='paper', y0=0.8, y1=1, yref='paper',
-        fillcolor='white', line=dict(color='black', width=1)
-    )
-    fig.add_shape(
-        type='line',
-        x0=0.06, x1=0.12, xref='paper', y0=0.95, y1=0.95, yref='paper',
-        line=dict(color='red', width=2, dash='dash')
-    )
-    
-    fig.add_annotation(
-        text='Début COVID', xref='paper', yref='paper',
-        x=0.12, y=0.98, showarrow=False
-    )
-    
-    fig.add_shape(
-        type='line',
-        x0=0.06, x1=0.12, xref='paper', y0=0.85, y1=0.85, yref='paper',
-        line=dict(color='green', width=2, dash='dash')
-    )
-    
-    fig.add_annotation(
-        text='Fin COVID', xref='paper', yref='paper',
-        x=0.12, y=0.88, showarrow=False
-    )
-    
-    
-    
-    
+    fig.update_yaxes(showgrid=False,
+                     showline=True,
+                     mirror=True,
+                     linewidth=1,
+                     linecolor='black')
+
+    fig = add_covid_info(fig)
+
     
     
     if selected is None:
@@ -85,3 +59,46 @@ def get_figure(data, selected, year):
 
 
 
+
+def add_covid_info(fig):
+        # Display COVID lines
+    fig.add_shape(
+        type='line',
+        x0='2020-03-01', x1='2020-03-01', xref='x', y0=0, y1=1, yref='paper',
+        line=dict(color='red', width=2, dash='dash')
+    )
+
+    fig.add_shape(
+        type='line',
+        x0='2022-03-01', x1='2022-03-01', xref='x', y0=0, y1=1, yref='paper',
+        line=dict(color='green', width=2, dash='dash')
+    )
+    # Legend of the COVID lines
+    fig.add_shape(
+        type='rect',
+        x0=0.05, x1=0.28, xref='paper', y0=0.82, y1=0.95, yref='paper',
+        fillcolor='white', line=dict(color='black', width=1)
+    )
+    fig.add_shape(
+        type='line',
+        x0=0.06, x1=0.12, xref='paper', y0=0.91, y1=0.91, yref='paper',
+        line=dict(color='red', width=2, dash='dash')
+    )
+    
+    fig.add_annotation(
+        text='Début COVID', xref='paper', yref='paper',
+        x=0.12, y=0.94, showarrow=False
+    )
+    
+    fig.add_shape(
+        type='line',
+        x0=0.06, x1=0.12, xref='paper', y0=0.85, y1=0.85, yref='paper',
+        line=dict(color='green', width=2, dash='dash')
+    )
+    
+    fig.add_annotation(
+        text='Fin COVID', xref='paper', yref='paper',
+        x=0.12, y=0.88, showarrow=False
+    )
+    
+    return fig
